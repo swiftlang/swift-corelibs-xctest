589b23d444495fc68592281c7837de918669d43f
Brian Gesiak <modocache@gmail.com>
2015-12-20 03:16:39 -0500

    [Tests] Add functional tests
    Adds a test runner (lit) that compares annotations in the source code of an
    XCTest file to actual output when that source code is compiled and run.
    This acts as a regression test suite for the project.
    
    - Because FileCheck is not available unless LLVM is built, includes an
      XCTest-specific version of FileCheck called `xctest_checker`. This is
      used by the lit tests in order to verify the output of XCTest runs.
    - Adds a test case that verifies the output of a single failing test
      case.
    - To run the tests on Linux, adds a `--test` option to
      `build_script.py`.
    - To run the tests on OS X, adds a `SwiftXCTestFunctionalTests` target
      to the Xcode project.

---
+++ Tests/Functional/xctest_checker/tests/test_compare.py
@@ -0,0 +1,41 @@
+import tempfile
+import unittest
+
+from xctest_checker import compare
+
+
+def _tmpfile(content):
+    """Returns the path to a temp file with the given contents."""
+    tmp = tempfile.mkstemp()[1]
+    with open(tmp, 'w') as f:
+        f.write(content)
+    return tmp
+
+
+class CompareTestCase(unittest.TestCase):
+    def test_no_match_raises(self):
+        actual = _tmpfile('foo\nbar\nbaz\n')
+        expected = _tmpfile('c: foo\nc: baz\nc: bar\n')
+        with self.assertRaises(AssertionError):
+            compare.compare(actual, expected, check_prefix='c: ')
+
+    def test_too_few_expected_raises(self):
+        actual = _tmpfile('foo\nbar\nbaz\n')
+        expected = _tmpfile('c: foo\nc: bar\n')
+        with self.assertRaises(AssertionError):
+            compare.compare(actual, expected, check_prefix='c: ')
+
+    def test_too_many_expected_raises(self):
+        actual = _tmpfile('foo\nbar\n')
+        expected = _tmpfile('c: foo\nc: bar\nc: baz\n')
+        with self.assertRaises(AssertionError):
+            compare.compare(actual, expected, check_prefix='c: ')
+
+    def test_match_does_not_raise(self):
+        actual = _tmpfile('foo\nbar\nbaz\n')
+        expected = _tmpfile('c: foo\nc: bar\nc: baz\n')
+        compare.compare(actual, expected, check_prefix='c: ')
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Tests/Functional/xctest_checker/xctest_checker.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import xctest_checker
+
+if __name__ == '__main__':
+    xctest_checker.main()
