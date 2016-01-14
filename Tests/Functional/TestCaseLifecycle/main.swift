c72f22a2ae6ded10d99046bbba4ba97b74084c8c
Brian Croom <brian.s.croom@gmail.com>
2016-01-13 21:12:03 -0500

    Add functional test for the current behavior of setUp() and tearDown()

---
+++ Tests/Functional/TestCaseLifecycle/main.swift
@@ -0,0 +1,42 @@
+// RUN: %{swiftc} %s -o %{built_tests_dir}/TestCaseLifecycle
+// RUN: %{built_tests_dir}/TestCaseLifecycle > %t || true
+// RUN: %{xctest_checker} %t %s
+// CHECK: Test Case 'SetUpTearDownTestCase.test_hasValueFromSetUp' started.
+// CHECK: In setUp\(\)
+// CHECK: In test_hasValueFromSetUp\(\)
+// CHECK: In tearDown\(\)
+// CHECK: Test Case 'SetUpTearDownTestCase.test_hasValueFromSetUp' passed \(\d+\.\d+ seconds\).
+// CHECK: Executed 1 test, with 0 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: Total executed 1 test, with 0 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+
+#if os(Linux) || os(FreeBSD)
+    import XCTest
+#else
+    import SwiftXCTest
+#endif
+
+class SetUpTearDownTestCase: XCTestCase {
+    var allTests: [(String, () throws -> Void)] {
+        return [
+            ("test_hasValueFromSetUp", test_hasValueFromSetUp),
+        ]
+    }
+
+    var value = 0
+
+    func setUp() {
+        print("In \(__FUNCTION__)")
+        value = 42
+    }
+
+    func tearDown() {
+        print("In \(__FUNCTION__)")
+    }
+
+    func test_hasValueFromSetUp() {
+        print("In \(__FUNCTION__)")
+        XCTAssertEqual(value, 42)
+    }
+}
+
+XCTMain([SetUpTearDownTestCase()])
