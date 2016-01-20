b1f40a97a882757086bf130b0fd991191ab4c10b
Brian Croom <brian.s.croom@gmail.com>
2016-01-20 16:43:37 -0500

    Add failing test for a new test case instance being used for each test
    This matches Darwin XCTest behavior

--- Tests/Functional/TestCaseLifecycle/main.swift
+++ Tests/Functional/TestCaseLifecycle/main.swift
@@ -7,7 +7,12 @@
 // CHECK: In tearDown\(\)
 // CHECK: Test Case 'SetUpTearDownTestCase.test_hasValueFromSetUp' passed \(\d+\.\d+ seconds\).
 // CHECK: Executed 1 test, with 0 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
-// CHECK: Total executed 1 test, with 0 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: Test Case 'NewInstanceForEachTestTestCase.test_hasInitializedValue' started.
+// CHECK: Test Case 'NewInstanceForEachTestTestCase.test_hasInitializedValue' passed \(\d+\.\d+ seconds\).
+// CHECK: Test Case 'NewInstanceForEachTestTestCase.test_hasInitializedValueInAnotherTest' started.
+// CHECK: Test Case 'NewInstanceForEachTestTestCase.test_hasInitializedValueInAnotherTest' passed \(\d+\.\d+ seconds\).
+// CHECK: Executed 2 tests, with 0 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: Total executed 3 tests, with 0 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
 
 #if os(Linux) || os(FreeBSD)
     import XCTest
@@ -41,4 +46,27 @@ class SetUpTearDownTestCase: XCTestCase {
     }
 }
 
-XCTMain([SetUpTearDownTestCase()])
+class NewInstanceForEachTestTestCase: XCTestCase {
+    var allTests: [(String, () throws -> Void)] {
+        return [
+            ("test_hasInitializedValue", test_hasInitializedValue),
+            ("test_hasInitializedValueInAnotherTest", test_hasInitializedValueInAnotherTest),
+        ]
+    }
+
+    var value = 1
+
+    func test_hasInitializedValue() {
+        XCTAssertEqual(value, 1)
+        value += 1
+    }
+
+    func test_hasInitializedValueInAnotherTest() {
+        XCTAssertEqual(value, 1)
+    }
+}
+
+XCTMain([
+    SetUpTearDownTestCase(),
+    NewInstanceForEachTestTestCase()
+])
