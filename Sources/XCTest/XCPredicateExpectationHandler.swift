4cd617b3c002b8c5751da54e007022c18f054436
Kyle Yoon <apps.yoon@gmail.com>
2016-05-03 23:43:29 -0500

    [XCTestExpectation] Adding expectationForPredicate constructor - Added class XCPredicateExpectation - Added typealias for XCPredicateExpectationHandler - Added tests for the expectation and handler

---
+++ Sources/XCTest/XCPredicateExpectationHandler.swift
@@ -0,0 +1,25 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//
+//  XCPredicateExpectationHandler.swift
+//  A closure invoked by XCTestCase when a predicate for the expectation is
+//  evaluated with a given object.
+//
+
+#if os(Linux) || os(FreeBSD)
+    import Foundation
+#else
+    import SwiftFoundation
+#endif
+
+/// A block to be invoked when evaluating the predicate against the object 
+/// returns true. If the block is not provided the first successful evaluation 
+/// will fulfill the expectation. If provided, the handler can override that 
+/// behavior which leaves the caller responsible for fulfilling the expectation.
+public typealias XCPredicateExpectationHandler = (Void) -> (Bool)
---
+++ Tests/Functional/Asynchronous/Predicates/Handler/main.swift
@@ -0,0 +1,74 @@
+// RUN: %{swiftc} %s -o %{built_tests_dir}/Asynchronous-Predicates-Handler
+// RUN: %{built_tests_dir}/Asynchronous-Predicates-Handler > %t || true
+// RUN: %{xctest_checker} %t %s
+
+#if os(Linux) || os(FreeBSD)
+    import XCTest
+    import Foundation
+#else
+    import SwiftXCTest
+    import SwiftFoundation
+#endif
+
+// CHECK: Test Suite 'All tests' started at \d+:\d+:\d+\.\d+
+// CHECK: Test Suite '.*\.xctest' started at \d+:\d+:\d+\.\d+
+
+// CHECK: Test Suite 'PredicateHandlerTestCase' started at \d+:\d+:\d+\.\d+
+class PredicateHandlerTestCase: XCTestCase {
+    // CHECK: Test Case 'PredicateHandlerTestCase.test_predicateIsTrue_handlerReturnsTrue_passes' started at \d+:\d+:\d+\.\d+
+    // CHECK: Test Case 'PredicateHandlerTestCase.test_predicateIsTrue_handlerReturnsTrue_passes' passed \(\d+\.\d+ seconds\).
+    func test_predicateIsTrue_handlerReturnsTrue_passes() {
+        let predicate = NSPredicate(value: true)
+        let object = NSObject()
+        self.expectation(for: predicate, evaluatedWith: object, handler: { _ in
+            return true
+        })
+        waitForExpectations(withTimeout: 0.1)
+    }
+    // CHECK: Test Case 'PredicateHandlerTestCase.test_predicateIsTrue_handlerReturnsFalse_fails' started at \d+:\d+:\d+\.\d+
+    // CHECK: .*/Tests/Functional/Asynchronous/Predicates/Handler/main.swift:37: error: PredicateHandlerTestCase.test_predicateIsTrue_handlerReturnsFalse_fails : Asynchronous wait failed - Exceeded timeout of 0.1 seconds, with unfulfilled expectations: Expect `<NSPredicate: 0x[0-9a-fA-F]{1,16}>` for object <NSObject: 0x[0-9a-fA-F]{1,16}>
+    // CHECK: Test Case 'PredicateHandlerTestCase.test_predicateIsTrue_handlerReturnsFalse_fails' failed \(\d+\.\d+ seconds\).
+    func test_predicateIsTrue_handlerReturnsFalse_fails() {
+        let predicate = NSPredicate(value: true)
+        let object = NSObject()
+        self.expectation(for: predicate, evaluatedWith: object, handler: { _ in
+            return false
+        })
+        waitForExpectations(withTimeout: 0.1)
+    }
+    
+    // CHECK: Test Case 'PredicateHandlerTestCase.test_predicateIsTrueAfterTimeout_handlerIsNotCalled_fails' started at \d+:\d+:\d+\.\d+
+    // CHECK: .*/Tests/Functional/Asynchronous/Predicates/Handler/main.swift:55: error: PredicateHandlerTestCase.test_predicateIsTrueAfterTimeout_handlerIsNotCalled_fails : Asynchronous wait failed - Exceeded timeout of 0.1 seconds, with unfulfilled expectations: Expect `<NSPredicate: 0x[0-9a-fA-F]{1,16}>` for object \d{4}-\d{2}-\d{2} \d+:\d+:\d+ \+\d+
+    // CHECK: Test Case 'PredicateHandlerTestCase.test_predicateIsTrueAfterTimeout_handlerIsNotCalled_fails' failed \(\d+\.\d+ seconds\).
+    func test_predicateIsTrueAfterTimeout_handlerIsNotCalled_fails() {
+        let halfSecLaterDate = NSDate(timeIntervalSinceNow: 0.2)
+        let predicate = NSPredicate(block: { evaluatedObject, bindings in
+            if let evaluatedDate = evaluatedObject as? NSDate {
+                return evaluatedDate.compare(NSDate()) == NSComparisonResult.OrderedAscending
+            }
+            return false
+        })
+        expectation(for: predicate, evaluatedWith: halfSecLaterDate, handler: { _ in
+            XCTFail("Should not call the predicate expectation handler")
+            return true
+        })
+        waitForExpectations(withTimeout: 0.1, handler: nil)
+    }
+    
+    static var allTests: [(String, PredicateHandlerTestCase -> () throws -> Void)] {
+        return [
+                   ("test_predicateIsTrue_handlerReturnsTrue_passes", test_predicateIsTrue_handlerReturnsTrue_passes),
+                   ("test_predicateIsTrue_handlerReturnsFalse_fails", test_predicateIsTrue_handlerReturnsFalse_fails),
+                   ("test_predicateIsTrueAfterTimeout_handlerIsNotCalled_fails", test_predicateIsTrueAfterTimeout_handlerIsNotCalled_fails),
+        ]
+    }
+}
+
+// CHECK: Test Suite 'PredicateHandlerTestCase' failed at \d+:\d+:\d+\.\d+
+// CHECK: \t Executed 3 tests, with 2 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+XCTMain([testCase(PredicateHandlerTestCase.allTests)])
+
+// CHECK: Test Suite '.*\.xctest' failed at \d+:\d+:\d+\.\d+
+// CHECK: \t Executed 3 tests, with 2 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: Test Suite 'All tests' failed at \d+:\d+:\d+\.\d+
+// CHECK: \t Executed 3 tests, with 2 failures \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
