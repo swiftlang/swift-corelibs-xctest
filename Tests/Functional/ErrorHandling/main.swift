8a44de05c66b216ac6c3a538a66fbaba974fb62e
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2017-01-18 11:05:44 -0500

    Changed check statement for failure

--- Tests/Functional/ErrorHandling/main.swift
+++ Tests/Functional/ErrorHandling/main.swift
@@ -116,7 +116,7 @@ func test_shouldNotThrowErrorDefiningSuccess() {
 }
 
 // CHECK: Test Case 'ErrorHandling.test_shouldThrowErrorDefiningError' started at \d+:\d+:\d+\.\d+
-// CHECK: .*/ErrorHandling/main.swift:[[@LINE+3]]: error: ErrorHandling.test_shouldThrowErrorDefiningFailure : XCTAssertNoThrow failed: threw error -
+// CHECK: .*/ErrorHandling/main.swift:[[@LINE+3]]: error: ErrorHandling.test_shouldThrowErrorDefiningFailure : XCTAssertNoThrow threw error
 // CHECK: Test Case 'ErrorHandling.test_shouldThrowErrorDefiningFailure' failed \(\d+\.\d+ seconds\)
 func test_shouldThrowErrorDefiningFailure() {
     XCTAssertNoThrow(try functionThatDoesThrowError())
