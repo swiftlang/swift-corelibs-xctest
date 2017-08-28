6e0af61ed7fb417f438af6d75ee8abf5282df72d
John Holdsworth <github@johnholdsworth.com>
2017-08-28 12:44:18 +0100

    For testing on Android

--- Package.swift
+++ Package.swift
@@ -1,3 +1,23 @@
+// swift-tools-version:4.0
+//
+// To build with auto-linking of the .swiftmodule use:
+// $ swift build -Xswiftc -module-link-name -Xswiftc XCTest
+//
+
 import PackageDescription
 
-let package = Package(name: "XCTest")
+let package = Package(
+    name: "XCTest",
+    products: [
+        .library(
+            name: "XCTest",
+            type: .dynamic,
+            targets: ["XCTest"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "XCTest", dependencies: [], path: "Sources"),
+    ]
+)
