6cb41b95d1f1a0c0bbc7b658943834de82b6fb85
Stuart Montgomery <smontgomery@apple.com>
2018-08-03 10:49:40 -0500

    Make NSPredicate expectation constructor API accept nil object
    - Change the object parameter of `XCTestCase.expectation(for:evaluatedWith:handler:)` from
      type `AnyObject` to `Any?` with a default value of `nil`. This more closely matches the
      type of the object parameter from the NSPredicate initializer.
    - Modify the internally-formatted `description` string to match ObjC XCTest implementation.
    - Update and add new unit tests

--- Tests/Functional/Asynchronous/Predicates/Expectations/main.swift
+++ Tests/Functional/Asynchronous/Predicates/Expectations/main.swift
@@ -13,66 +13,100 @@
 
 // CHECK: Test Suite 'PredicateExpectationsTestCase' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
 class PredicateExpectationsTestCase: XCTestCase {
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTruePredicateAndObject_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTruePredicateAndObject_passes' passed \(\d+\.\d+ seconds\)
-    func test_immediatelyTruePredicateAndObject_passes() {
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTruePredicate_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTruePredicate_passes' passed \(\d+\.\d+ seconds\)
+    func test_immediatelyTruePredicate_passes() {
         let predicate = NSPredicate(value: true)
-        let object = NSObject()
-        expectation(for: predicate, evaluatedWith: object)
+        expectation(for: predicate)
         waitForExpectations(timeout: 0.1)
     }
 
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyFalsePredicateAndObject_fails' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-    // CHECK: .*/Tests/Functional/Asynchronous/Predicates/Expectations/main.swift:[[@LINE+6]]: error: PredicateExpectationsTestCase.test_immediatelyFalsePredicateAndObject_fails : Asynchronous wait failed - Exceeded timeout of 0.1 seconds, with unfulfilled expectations: Expect `<NSPredicate: 0x[0-9A-Fa-f]{1,16}>` for object <NSObject: 0x[0-9A-Fa-f]{1,16}>
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyFalsePredicateAndObject_fails' failed \(\d+\.\d+ seconds\)
-    func test_immediatelyFalsePredicateAndObject_fails() {
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyFalsePredicate_fails' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
+    // CHECK: .*/Tests/Functional/Asynchronous/Predicates/Expectations/main.swift:[[@LINE+5]]: error: PredicateExpectationsTestCase.test_immediatelyFalsePredicate_fails : Asynchronous wait failed - Exceeded timeout of 0.1 seconds, with unfulfilled expectations: Expect predicate `<NSPredicate: 0x[0-9A-Fa-f]{1,16}>`
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyFalsePredicate_fails' failed \(\d+\.\d+ seconds\)
+    func test_immediatelyFalsePredicate_fails() {
         let predicate = NSPredicate(value: false)
-        let object = NSObject()
-        expectation(for: predicate, evaluatedWith: object)
+        expectation(for: predicate)
         waitForExpectations(timeout: 0.1)
     }
 
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_delayedTruePredicateAndObject_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_delayedTruePredicateAndObject_passes' passed \(\d+\.\d+ seconds\)
-    func test_delayedTruePredicateAndObject_passes() {
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_delayedTruePredicate_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_delayedTruePredicate_passes' passed \(\d+\.\d+ seconds\)
+    func test_delayedTruePredicate_passes() {
         var didEvaluate = false
         let predicate = NSPredicate(block: { evaluatedObject, bindings in
+            XCTAssertNil(evaluatedObject)
             defer { didEvaluate = true }
             return didEvaluate
         })
-        expectation(for: predicate, evaluatedWith: NSObject())
+        expectation(for: predicate)
         waitForExpectations(timeout: 0.1)
     }
     
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTrueDelayedFalsePredicateAndObject_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTrueDelayedFalsePredicateAndObject_passes' passed \(\d+\.\d+ seconds\)
-    func test_immediatelyTrueDelayedFalsePredicateAndObject_passes() {
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTrueDelayedFalsePredicate_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_immediatelyTrueDelayedFalsePredicate_passes' passed \(\d+\.\d+ seconds\)
+    func test_immediatelyTrueDelayedFalsePredicate_passes() {
         var didEvaluate = false
         let predicate = NSPredicate(block: { evaluatedObject, bindings in
+            XCTAssertNil(evaluatedObject)
             defer { didEvaluate = true }
             return !didEvaluate
         })
-        expectation(for: predicate, evaluatedWith: NSObject())
+        expectation(for: predicate)
         XCTAssertTrue(didEvaluate)
         
         waitForExpectations(timeout: 0.1)
     }
+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_blockPredicateWithNilObject_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_blockPredicateWithNilObject_passes' passed \(\d+\.\d+ seconds\)
+    func test_blockPredicateWithNilObject_passes() {
+        var flag = false
+        let predicate = NSPredicate(block: { _, _ in
+            return flag
+        })
+        expectation(for: predicate, evaluatedWith: nil)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            flag = true
+        }
+        waitForExpectations(timeout: 1)
+        XCTAssertTrue(flag)
+    }
+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_blockPredicateWithObject_passes' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
+    // CHECK: Test Case 'PredicateExpectationsTestCase.test_blockPredicateWithObject_passes' passed \(\d+\.\d+ seconds\)
+    func test_blockPredicateWithObject_passes() {
+        class Foo { var x = false }
+        let foo = Foo()
+        let predicate = NSPredicate(block: { evaluatedObject, _ in
+            guard let object = evaluatedObject as? Foo else { return false }
+            return object.x
+        })
+        expectation(for: predicate, evaluatedWith: foo)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            foo.x = true
+        }
+        waitForExpectations(timeout: 1)
+        XCTAssertTrue(foo.x)
+    }
     
     static var allTests = {
         return [
-                   ("test_immediatelyTruePredicateAndObject_passes", test_immediatelyTruePredicateAndObject_passes),
-                   ("test_immediatelyFalsePredicateAndObject_fails", test_immediatelyFalsePredicateAndObject_fails),
-                   ("test_delayedTruePredicateAndObject_passes", test_delayedTruePredicateAndObject_passes),
-                   ("test_immediatelyTrueDelayedFalsePredicateAndObject_passes", test_immediatelyTrueDelayedFalsePredicateAndObject_passes),
+                   ("test_immediatelyTruePredicate_passes", test_immediatelyTruePredicate_passes),
+                   ("test_immediatelyFalsePredicate_fails", test_immediatelyFalsePredicate_fails),
+                   ("test_delayedTruePredicate_passes", test_delayedTruePredicate_passes),
+                   ("test_immediatelyTrueDelayedFalsePredicate_passes", test_immediatelyTrueDelayedFalsePredicate_passes),
+                   ("test_blockPredicateWithNilObject_passes", test_blockPredicateWithNilObject_passes),
+                   ("test_blockPredicateWithObject_passes", test_blockPredicateWithObject_passes),
         ]
     }()
 }
 
 // CHECK: Test Suite 'PredicateExpectationsTestCase' failed at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-// CHECK: \t Executed 4 tests, with 1 failure \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: \t Executed 6 tests, with 1 failure \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
 XCTMain([testCase(PredicateExpectationsTestCase.allTests)])
 
 // CHECK: Test Suite '.*\.xctest' failed at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-// CHECK: \t Executed 4 tests, with 1 failure \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: \t Executed 6 tests, with 1 failure \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
 // CHECK: Test Suite 'All tests' failed at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-// CHECK: \t Executed 4 tests, with 1 failure \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
+// CHECK: \t Executed 6 tests, with 1 failure \(0 unexpected\) in \d+\.\d+ \(\d+\.\d+\) seconds
