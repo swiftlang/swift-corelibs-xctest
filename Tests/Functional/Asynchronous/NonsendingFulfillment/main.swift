384ee455d6ddf9e996281503fa954be7f04f00cf
Jonathan Grynspan <jgrynspan@apple.com>
2026-03-04 21:03:06 +0000

    Fix `fulfillment(of:)` not playing well with sendability.
    Resolves #507.

---
+++ Tests/Functional/Asynchronous/NonsendingFulfillment/main.swift
@@ -0,0 +1,18 @@
+// RUN: %{swiftc} %s -warn-concurrency -warnings-as-errors
+
+#if os(macOS)
+    import SwiftXCTest
+#else
+    import XCTest
+#endif
+
+final class NonisolatedExpectationTests: XCTestCase {
+    @MainActor func testIt() async {
+        let something = expectation(description: "bla")
+        Task {
+            try? await Task.sleep(for: .seconds(1.0))
+            something.fulfill()
+        }
+        await fulfillment(of: [something], timeout: 2.0)
+    }
+}
