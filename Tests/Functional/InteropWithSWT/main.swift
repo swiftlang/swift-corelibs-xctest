0a16eaf242722008ea6ae2ff66616d684b22f8cf
Jerry Chen <jerryjrchen@apple.com>
2026-04-09 12:18:19 -0700

    Fix expected assert message
    We explicitly concatenate together all Interop.Event messages.

--- Tests/Functional/InteropWithSWT/main.swift
+++ Tests/Functional/InteropWithSWT/main.swift
@@ -18,12 +18,12 @@ class InteropWithSWTTestCase: XCTestCase {
     // CHECK: .*[/\\]InteropWithSWT[/\\]main.swift:[[@LINE+3]]: error: InteropWithSWTTestCase.test_records_issue : Issue recorded: Interop recorded issue failure
     // CHECK: Test Case 'InteropWithSWTTestCase.test_records_issue' failed \(\d+\.\d+ seconds\)
     func test_records_issue() {
-        // #expect(Bool(false))
         Issue.record("Interop recorded issue failure")
     }
 
     // CHECK: Test Case 'InteropWithSWTTestCase.test_assertion_fails' started at \d+-\d+-\d+ \d+:\d+:\d+\.\d+
-    // CHECK: .*[/\\]InteropWithSWT[/\\]main.swift:[[@LINE+3]]: error: InteropWithSWTTestCase.test_assertion_fails : Expectation failed: Bool\(false\)
+    // CHECK: .*[/\\]InteropWithSWT[/\\]main.swift:[[@LINE+4]]: error: InteropWithSWTTestCase.test_assertion_fails : Expectation failed: Bool\(false\): Bool\(false\) → false
+    // CHECK: false → \(\)
     // CHECK: Test Case 'InteropWithSWTTestCase.test_assertion_fails' failed \(\d+\.\d+ seconds\)
     func test_assertion_fails() {
         #expect(Bool(false))
